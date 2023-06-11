34079f697c22ede9851cbcf04bfec5d15b2ee3e7
LucasRiboli <lucasriboli57@gmail.com>
2023-06-11 17:13:46 -0300

    grammar check

--- src/function_commands/commands_note.py
+++ src/function_commands/commands_note.py
@@ -1,16 +1,39 @@
 import os
 from datetime import datetime
+import requests
+import json
 
-
-def check_exists(file_name):
+def __check_exists(file_name):
     all_directory_files = os.listdir()
     for file in all_directory_files:
         if f"{file_name}.txt" == file:
             return "\n"
-    return f"Notas do Dia {file_name} \n"
+    return f"- Notas do Dia {file_name} \n"
 
-def take_note(note):
-    file_name = datetime.now().date()
-    note_sufix = check_exists(file_name)
-    with open(f"{file_name}.txt", "a") as data:
+def __grammatic_help(note):
+    headers = {
+                'content-type': '',
+                'X-RapidAPI-Key': '',
+                'X-RapidAPI-Host': ''
+            }
+    payload = {
+        "text" : note,
+        "language": "pt-BR"
+    }
+    try:
+        response = requests.post(url="https://dnaber-languagetool.p.rapidapi.com/v2/check", headers=headers, data=json.dumps(payload))
+        print(response)
+        return response
+    except requests.RequestException as e:
+        print(e)
+        return note  
+    
+def take_note(note: str, note_name = datetime.now().date()):
+    note_sufix = __check_exists(note_name)
+    note = __grammatic_help(note)
+    with open(f"{note_name}.txt", "a") as data:
         data.write(f"{note_sufix}{note}")
+
+def read_note(name_file = datetime.now().date()):
+    with open(f"{name_file}.txt", "r") as data:
+        return data.read()
--- src/main.py
+++ src/main.py
@@ -1,15 +1,24 @@
 from typing import Optional
 
 import typer
-import src.function_commands.commands_note as notes
+import function_commands.commands_note as notes
 
 app = typer.Typer()
 
 @app.command()
-def take_note(note):
-    notes.take_note(note)
+def take_note(note: str, note_name: Optional[str] = None):
+    if note_name:
+        notes.take_note(note, note_name)
+    else:
+        notes.take_note(note)
     typer.echo("Nota salva com sucesso")
-    
+
+@app.command()
+def see_note(date_file: Optional[str] = None):
+    if date_file:
+        typer.echo(notes.read_note(date_file))
+    else:
+        typer.echo(notes.read_note())
 
 if __name__ == "__main__":
     app()
